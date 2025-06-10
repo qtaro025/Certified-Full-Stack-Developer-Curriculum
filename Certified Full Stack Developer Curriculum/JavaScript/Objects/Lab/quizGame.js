8c242732655fd2b75ae2f3a8fbc91a36e1fb7b2c
Quentin Quarles <quentina@yamaha-robotics.com>
2025-06-10 22:40:59 +0900

    Partial completion of Objects Section

---
+++ Certified Full Stack Developer Curriculum/JavaScript/Objects/Lab/quizGame.js
@@ -0,0 +1,72 @@
+/**
+    Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+    User Stories:
+    -------------
+    1. You should create an array named questions.
+    2. The questions array should contain at least five objects, each having the keys category, question, choices, and answer.
+    3. The category key should have the value of a string representing a question category.
+    4. The question key should have the value of a string representing a question.
+    5. The choices key should have the value of an array containing three strings, which are alternative answers to the question.
+    6. The answer key should have the value of a string, representing the correct answer to the question. Also, the value of answer should be included in the choices array.
+    7. You should have a function named getRandomQuestion that takes an array of questions as a parameter and returns a random question object from the array.
+    8. You should have a function named getRandomComputerChoice that takes the array of the available choices as a parameter, and returns a random answer to the selected question.
+    9. You should have a function named getResults that takes the question object as the first parameter and the computer's choice as the second parameter. The function should return The computer's choice is correct! if the answer is correct. Otherwise, it returns The computer's choice is wrong. The correct answer is: <correct-answer>, where <correct-answer> is the value of the correct answer to the chosen question.
+
+ */
+
+const questions = [];
+
+const getRandomQuestion = (arr) => {
+  const min = 0;
+  const max = arr.lengh
+    const questionIdx = Math.floor(
+    Math.random() * (0, arr.lengh)
+  )
+  return "";
+}
+
+const question1 = {
+  category: "U.S. History",
+  question: "Who is the first United States President?",
+  choices: ["Bill Clinton", "Barack Obama", "George Washington"],
+  answer: "George Washington",
+};
+
+const question2 = {
+  category: "California State Parks",
+  question: "Which California state park is Half Dome Located?",
+  choices: ["Yosemite National Park", "Sequoia National Park", "Kings Canyon National Park"],
+  answer: "Yosemite National Park",
+};
+
+const question3 = {
+  category: "University of California Nobel Laureates",
+  question: "Which University of California university has the highest number of Nobel Lauretes?",
+  choices: ["UC Irvine", "UC San Diego", "UC Berkeley"],
+  answer: "UC Berkeley",
+};
+
+const question4 = {
+  category: "U.S. Monuments",
+  question: "In which U.S. state is Mount Rushmore National Memorial Located?",
+  choices: ["North Dakota", "South Dakota", "Arizona"],
+  answer: "South Dakota",
+};
+
+const question5 = {
+  category: "UCSD Colleges",
+  question: "How many colleges exists at UCSD?",
+  choices: ["1", "8", "7"],
+  answer: "8",
+};
+
+questions.push(question1);
+questions.push(question2);
+questions.push(question3);
+questions.push(question4);
+questions.push(question5);
+
+
+
+
---
+++ Certified Full Stack Developer Curriculum/JavaScript/Objects/Workshop/Build a Recipe Tracker/recipeTracker.js
@@ -0,0 +1,91 @@
+const recipes = [];
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese",
+    "pancetta", "black pepper"],
+  ratings: [4, 5, 4, 5],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: "",
+  averageRating: null,
+};
+
+const recipe2 = {
+  name: "Chicken Curry",
+  ingredients: ["chicken breasts", "coconut milk", "curry powder", "onion", "garlic"],
+  ratings: [4, 5, 5, 5],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: '',
+  averageRating: null,
+};
+
+const recipe3 = {
+  name: 'Vegetable Stir Fry',
+  ingredients: ['broccoli', 'carrot', 'bell pepper'],
+  cookingTime: 15,
+  totalIngredients: null,
+  difficultyLevel: '',
+  ratings: [4, 3, 4, 5],
+  averageRating: null,
+};
+
+const getAverageRating = (arr) => {
+  return (arr[0] + arr[1] + arr[2] + arr[3]) / arr.length
+};
+
+const getTotalIngredients = arr => arr.length;
+
+const getDifficultyLevel = (num) => {
+  if (num <= 30) {
+    return "easy";
+  } else if (num <= 60) {
+    return "medium";
+  } else {
+    return "hard";
+  }
+};
+
+const recipe1Name = recipe1.name;
+const recipe2Name = recipe2["name"];
+const recipe1CookingTime = recipe1.cookingTime;
+const recipe2CookingTime = recipe2["cookingTime"];
+
+console.log(`recipe 1 name: ${recipe1Name}, cooking time: ${recipe1CookingTime}`);
+console.log(`recipe 2 name: ${recipe2Name}, cooking time: ${recipe2CookingTime}`);
+
+// Push the three recipes into the recipes array
+recipes.push(recipe1);
+recipes.push(recipe2);
+recipes.push(recipe3);
+
+console.log(getAverageRating(recipe1.ratings));
+console.log(getAverageRating(recipe2.ratings));
+console.log(getAverageRating(recipe3.ratings));
+
+console.log(getTotalIngredients(recipe1.ingredients));
+console.log(getAverageRating(recipe2.ingredients));
+console.log(getAverageRating(recipe3.ingredients));
+
+const recipe1AverageRating = getAverageRating(recipe1.ratings);
+const recipe1TotalIngredients = getTotalIngredients(recipe1.ingredients);
+const recipe1DifficultyLevel = getDifficultyLevel(recipe1.difficultyLevel);
+
+console.log(recipe1AverageRating);
+console.log(recipe1TotalIngredients);
+console.log(recipe1DifficultyLevel);
+
+recipe1.averageRating = getAverageRating(recipe1.ratings);
+recipe1.totalIngredients = getTotalIngredients(recipe1.ingredients);
+recipe1["difficultyLevel"] = getDifficultyLevel(recipe1.difficultyLevel);
+
+recipe2.averageRating = getAverageRating(recipe2.ratings);
+recipe2.totalIngredients = getTotalIngredients(recipe2.ingredients);
+recipe2["difficultyLevel"] = getDifficultyLevel(recipe2.cookingTime);
+
+recipe3.averageRating = getAverageRating(recipe3.ratings);
+recipe3.totalIngredients = getTotalIngredients(recipe3.ingredients)
+recipe3.difficultyLevel = getDifficultyLevel(recipe3.cookingTime)
+
+console.log(recipes);
