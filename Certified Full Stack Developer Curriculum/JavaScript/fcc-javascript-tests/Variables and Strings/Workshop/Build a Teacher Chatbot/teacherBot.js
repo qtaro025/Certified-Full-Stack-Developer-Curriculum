e4d129fd46efbcc18962b943187d67b1d98763d3
Quentin Quarles <qtaro025@gmail.com>
2025-10-26 18:29:41 +0900

    Updated Build a Teacher Chatbot Workshop code.

--- Certified Full Stack Developer Curriculum/JavaScript/fcc-javascript-tests/Variables and Strings/Workshop/Build a Teacher Chatbot/teacherBot.js
+++ Certified Full Stack Developer Curriculum/JavaScript/fcc-javascript-tests/Variables and Strings/Workshop/Build a Teacher Chatbot/teacherBot.js
@@ -1,44 +1,47 @@
 console.log("Hi there!");
 
-let botName = "teacherBot";
-let greeting = `My name is ${botName}.`;
-console.log(greeting);
+let teacherBotName = "teacherBot";
+let teacherBotGreeting = `My name is ${teacherBotName}.`;
+console.log(teacherBotGreeting);
 
 let subject = "JavaScript";
 let topic = "strings";
 
 let sentence = `Today, you will learn about ${topic} in ${subject}.`;
 console.log(sentence);
+console.log("");
 
 let strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
 console.log(strLengthIntro);
-console.log(subject.length);
+console.log(`Length of ${subject}: ${subject.length}`);
+console.log("");
 
 console.log(
   `Here is an example of using the length property on the word ${topic}.`
 );
-console.log(topic.length);
+console.log(`Length of ${topic}: ${topic.length}`);
+console.log("");
 
 console.log(
   `Here is an example of accessing the first letter in the word ${subject}.`
 );
-
-console.log(subject[0]);
+console.log(`FIrst letter of ${subject}: ${subject[0]}`);
+console.log("");
 
 console.log(`Here is an example of accessing the second letter in the word ${subject}.`);
-console.log(subject[1]);
+console.log(`Second letter of ${subject}: ${subject[1]}`);
+console.log("");
 
 console.log(`Here is an example of accessing the last letter in the word ${subject}.`);
-
 let lastCharacter = subject[subject.length - 1];
-console.log(lastCharacter);
+console.log(`Last letter of ${subject}: ${lastCharacter}`);
+console.log("");
 
 let learningIsFunSentence = "Learning is fun.";
 console.log("Here are examples of finding the positions of substrings in the sentence.");
-
-console.log(learningIsFunSentence.indexOf("Learning"));
-
-console.log(learningIsFunSentence.indexOf("fun"));
-console.log(learningIsFunSentence.indexOf("learning"));
+console.log(`Index of Learning: ${learningIsFunSentence.indexOf("Learning")}`);
+console.log(`Index of fun: ${learningIsFunSentence.indexOf("fun")}`);
+console.log(`Index of learning: ${learningIsFunSentence.indexOf("learning")}`);
+console.log("");
 
 console.log("I hope you enjoyed learning today.");
